edccadaa916423c6769a596515ecc6b85f23c75f
Hugo González <114906900+HugoGonzalezSalas@users.noreply.github.com>
2023-10-17 09:23:08 +0200

    Update clientefailover.sh

--- contenidos/clientefailover.sh
+++ contenidos/clientefailover.sh
@@ -3,5 +3,18 @@ apt update
 apt upgrade
 
 cat <<EOF >>/etc/network/interfaces
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
 
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+#dhcp
+auto enp0s3
+iface enp0s3 inet dhcp
 EOF
+
+systemctl restart networking.service
