59de4abc8a88c55c25eb5157169f88589b790163
Hugo González <114906900+HugoGonzalezSalas@users.noreply.github.com>
2023-10-17 08:59:25 +0200

    Update and rename failover.sh to failover1.sh

---
+++ contenidos/failover1.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+apt update
+apt upgrade
+apt install isc-dhcp-server
+apt install iptables
+systemctl disable NetworkManager
+systemctl stop NetworkManager
+
+cat <<EOF >>/etc/network/interfaces
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto enp0s3
+iface enp0s3 inet static
+address 172.26.0.240
+netmask 255.255.0.0
+gateway 172.26.0.1
+
+auto enp0s8
+iface enp0s8 inet static
+address 192.168.0.2
+netmask 255.255.255.0
+EOF
+
+cat <<EOF >>/etc/isc-dhcp-server 
+# Defaults for isc-dhcp-server (sourced by /etc/init.d/isc-dhcp-server)
+
+# Path to dhcpd's config file (default: /etc/dhcp/dhcpd.conf).
+#DHCPDv4_CONF=/etc/dhcp/dhcpd.conf
+#DHCPDv6_CONF=/etc/dhcp/dhcpd6.conf
+
+# Path to dhcpd's PID file (default: /var/run/dhcpd.pid).
+#DHCPDv4_PID=/var/run/dhcpd.pid
+#DHCPDv6_PID=/var/run/dhcpd6.pid
+
+# Additional options to start dhcpd with.
+#	Don't use options -cf or -pf here; use DHCPD_CONF/ DHCPD_PID instead
+#OPTIONS=""
+
+# On what interfaces should the DHCP server (dhcpd) serve DHCP requests?
+#	Separate multiple interfaces with spaces, e.g. "eth0 eth1".
+INTERFACESv4="enp0s8"
+INTERFACESv6=""
+EOF
+
+cat <<EOF >>/etc/dhcp/dhcpd.conf 
+option domain-name "example.org";
+option domain-name-servers ns1.example.org, ns2.example.org;
+
+default-lease-time 600;
+max-lease-time 7200;
+
+# The ddns-updates-style parameter controls whether or not the serv>
+# attempt to do a DNS update when a lease is confirmed. We default >
+# behavior of the version 2 packages ('none', since DHCP v2 didn't
+# have support for DDNS.)
+ddns-update-style none;
+
+#failover
+
+failover peer "FAILOVER" {
+ primary;
+ address 192.168.0.2;
+ port 647;
+ peer address 192.168.0.3;
+ peer port 647;
+ max-unacked-updates 10;
+ max-response-delay 30;
+ load balance max seconds 3;
+ mclt 1800;
+ split 128;
+}
+
+subnet 192.168.0.0 netmask 255.255.255.0 {
+ option routers 192.168.0.2;
+ option domain-name-servers 8.8.8.8;
+ option subnet-mask 255.255.255.0;
+ option broadcast-address 192.168.0.255;
+ pool {
+  failover peer "FAILOVER";
+  range 192.168.0.5 192.168.0.50;
+  }
+}
+EOF
+
+ifdown enp0s3
+ifdown enp0s8
+ifup enp0s3
+ifup enp0s8
+systemctl restart networking.service
+systemctl restart isc-dhcp-server.service
+
+sysctl -w net.ipv4.ip_forward=1
+iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
+apt install iptables-persistent
+systemctl restart isc-dhcp-server.service
+
