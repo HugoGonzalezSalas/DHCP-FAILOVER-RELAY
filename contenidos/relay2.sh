cd6e0e7c616ac0a11275fa850af1aec9a5f1367a
Hugo González <114906900+HugoGonzalezSalas@users.noreply.github.com>
2023-10-17 09:50:06 +0200

    Update relay2.sh

--- contenidos/relay2.sh
+++ contenidos/relay2.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-apt update
-apt upgrade
-apt install isc-dhcp-server
+apt update 
+apt upgrade -y
+apt install isc-dhcp-server -y
+cat <<EOF >>/etc/network/interfaces
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
 
+source /etc/network/interfaces.d/*
 
+# The loopback network interface
+auto enp0s3
+iface enp0s3 inet dhcp
+
+auto enp0s8
+iface enp0s8 inet dhcp
+EOF
+
+cat <<EOF >>/etc/default/isc-dhcp-relay
+SERVERS="192.168.1.2"
+INTERFACES="enp0s8"
+OPTIONS=""
+EOF
+
+cat <<EOF >>/etc/sysctl.conf
+net.ipv4.ip_forward=1
+EOF
