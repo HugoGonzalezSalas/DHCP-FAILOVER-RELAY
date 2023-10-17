82d87a6a1a0f6f59fd402139e26c69552414c38b
Hugo González <114906900+HugoGonzalezSalas@users.noreply.github.com>
2023-10-17 09:38:38 +0200

    Update relay1.sh

--- contenidos/relay1.sh
+++ contenidos/relay1.sh
@@ -5,3 +5,72 @@ apt install isc-dhcp-server
 apt install iptables
 systemctl disable NetworkManager
 systemctl stop NetworkManager
+cat <<EOF >>/etc/dhcp/dhcpd.conf
+# dhcpd.conf
+#
+# Sample configuration file for ISC dhcpd
+#
+
+# option definitions common to all supported networks...
+option domain-name "example.org";
+option domain-name-servers ns1.example.org, ns2.example.org;
+
+default-lease-time 600;
+max-lease-time 7200;
+
+# The ddns-updates-style parameter controls whether or not the server will
+# attempt to do a DNS update when a lease is confirmed. We default to the
+# behavior of the version 2 packages ('none', since DHCP v2 didn't
+# have support for DDNS.)
+ddns-update-style none;
+
+subnet 192.168.1.0 netmask 255.255.255.0 {
+    range 192.168.1.5 192.168.1.40;
+    option routers 192.168.1.2;
+    option domain-name-servers 8.8.8.8;
+    option subnet-mask 255.255.255.0;
+    option broadcast-address 192.168.1.255;
+}
+EOF
+
+cat <<EOF >>/etc/network/interfaces
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto enp0s3
+iface enp0s3 inet static
+address 172.26.0.240
+netmask 255.255.0.0
+gateway 172.26.0.1
+
+auto enp0s8
+iface enp0s8 inet static
+address 192.168.1.2
+netmask 255.255.255.0
+EOF
+
+cat <<EOF >>/etc/default/isc-dhcp-server 
+# Defaults for isc-dhcp-server (sourced by /etc/init.d/isc-dhcp-server)
+
+# Path to dhcpd's config file (default: /etc/dhcp/dhcpd.conf).
+#DHCPDv4_CONF=/etc/dhcp/dhcpd.conf
+#DHCPDv6_CONF=/etc/dhcp/dhcpd6.conf
+
+# Path to dhcpd's PID file (default: /var/run/dhcpd.pid).
+#DHCPDv4_PID=/var/run/dhcpd.pid
+#DHCPDv6_PID=/var/run/dhcpd6.pid
+
+# Additional options to start dhcpd with.
+#       Don't use options -cf or -pf here; use DHCPD_CONF/ DHCPD_PID instead
+#OPTIONS=""
+
+# On what interfaces should the DHCP server (dhcpd) serve DHCP requests?
+#       Separate multiple interfaces with spaces, e.g. "eth0 eth1".
+INTERFACESv4="enp0s8"
+INTERFACESv6=""
+EOF
+
+
