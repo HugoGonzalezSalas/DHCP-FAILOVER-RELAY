b25aa05a61d31d9417d3f847fd5f64a5905c943b
Hugo González <114906900+HugoGonzalezSalas@users.noreply.github.com>
2023-10-17 08:46:12 +0200

    Update failover.sh

--- contenidos/failover.sh
+++ contenidos/failover.sh
@@ -4,3 +4,21 @@ apt upgrade
 apt install isc-dhcp-server
 apt install iptables
 
+cat <<EOF >>/etc/network/interfaces
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto enp0s3
+iface enp0s3 inet static
+address 172.26.0.240
+netmask 255.255.0.0
+gateway 172.26.0.1
+
+auto enp0s8
+iface enp0s8 inet static
+address 192.168.0.2
+netmask 255.255.255.0
+EOF
